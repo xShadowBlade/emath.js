1e6a3c2b642aafe0d32271feefcf26ca2d0aa377
zBlade <xshadowblade618@gmail.com>
2024-11-18 13:28:41 +0000

    feat(main): add setting Decimal to window

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,18 @@ export * from "./classes/numericalAnalysis/inverseFunction";
 export * from "./E/eMain";
 
 export type * from "./common/types";
+
+// Set `Decimal` to window (if window exists and Decimal is not already defined).
+import { Decimal } from "./E/e";
+
+/**
+ * The window object with the Decimal library.
+ * Used to set the Decimal library to the window object,
+ * so you can use it with libraries that depend on it.
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type WindowWithDecimal = typeof window & { Decimal: typeof Decimal };
+
+if (typeof window !== "undefined" && !(window as WindowWithDecimal).Decimal) {
+    (window as WindowWithDecimal).Decimal = Decimal;
+}
