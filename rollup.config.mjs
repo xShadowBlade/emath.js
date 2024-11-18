2d6e96959eb87745b7e196ddb617a1062f14f7b5
zBlade <xshadowblade618@gmail.com>
2024-11-18 13:41:39 +0000

    build: add mjs build option and minify for umd

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,14 +6,23 @@
 import resolve from "@rollup/plugin-node-resolve";
 import swc from "@rollup/plugin-swc";
 import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+/**
+ * The options for rollup.
+ * Also includes the type of the build in the `type` property
+ * (deleted before passing to rollup, used to determine if the build is for development or production)
+ * @typedef {import("rollup").RollupOptions & { type: "development" | "production" }} RollupOptions
+ */
 
 /**
  * An array of options for rollup
- * @type {import("rollup").RollupOptions[]}
+ * @type {RollupOptions[]}
  */
 const options = [
-    // Module "emath.js"
+    // UMD "emath.js" (production)
     {
+        type: "production",
         input: "./src/index.ts",
         output: {
             format: "umd",
@@ -21,8 +30,9 @@ const options = [
             file: "./dist/main/eMath.js",
         },
     },
-    // Module "emath.game.js"
+    // UMD "emath.game.js" (production)
     {
+        type: "production",
         input: "./src/game/index.ts",
         output: {
             format: "umd",
@@ -30,8 +40,9 @@ const options = [
             file: "./dist/game/eMath.game.js",
         },
     },
-    // Module "emath.presets.js"
+    // UMD "emath.presets.js" (production)
     {
+        type: "production",
         input: "./src/presets/index.ts",
         output: {
             format: "umd",
@@ -39,21 +50,41 @@ const options = [
             file: "./dist/presets/eMath.presets.js",
         },
     },
-];
 
-/**
- * The default options for development
- * @type {import("rollup").RollupOptions}
- */
-const optionsDev = {
-    external: ["reflect-metadata", "class-transformer"],
-};
+    // esm "emath.mjs" (development)
+    {
+        type: "development",
+        input: "./src/index.ts",
+        output: {
+            format: "esm",
+            file: "./dist/main/eMath.mjs",
+        },
+    },
+    // esm "emath.game.mjs" (development)
+    {
+        type: "development",
+        input: "./src/game/index.ts",
+        output: {
+            format: "esm",
+            file: "./dist/game/eMath.game.mjs",
+        },
+    },
+    // esm "emath.presets.mjs" (development)
+    {
+        type: "development",
+        input: "./src/presets/index.ts",
+        output: {
+            format: "esm",
+            file: "./dist/presets/eMath.presets.mjs",
+        },
+    },
+];
 
 /**
  * The plugins for rollup
  * @type {import("rollup").Plugin[]}
  */
-const plugins = [
+const defaultPlugins = [
     // For resolving "./file-name" instead of "./file-name.ts" (I did *not* spend 3+ hours on this)
     resolve({
         extensions: [".js", ".ts", ".tsx"],
@@ -70,15 +101,55 @@ const plugins = [
 ];
 
 /**
+ * The default options for development
+ * @type {import("rollup").RollupOptions}
+ */
+const optionsDev = {
+    external: ["reflect-metadata", "class-transformer"],
+    plugins: defaultPlugins,
+};
+
+/**
+ * The default options for production
+ * @type {import("rollup").RollupOptions}
+ */
+const optionsMin = {
+    // external: ["reflect-metadata", "class-transformer"],
+    plugins: [
+        ...defaultPlugins,
+        terser({
+            format: {
+                comments: false,
+            },
+        }),
+    ],
+};
+
+/**
  * The configuration object for rollup
  * @type {import("rollup").RollupOptions[]}
  */
 const config = options.map((option) => {
-    return {
-        ...optionsDev,
-        plugins,
+    /**
+     * The temporary options for the current option
+     * @type {RollupOptions}
+     */
+    const temporaryOptions = {
+        // If the type is development, use the development options, otherwise use the production options
+        ...(option.type === "development" ? optionsDev : optionsMin),
+
+        // Add the options
         ...option,
+
+        // Remove the type property
+        // type: undefined,
     };
+
+    // Delete the type property
+    // @ts-expect-error - Attempting to delete a property
+    delete temporaryOptions.type;
+
+    return temporaryOptions;
 });
 
 export default config;
