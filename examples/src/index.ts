ca4b9e0a742a48f5af90dbcb9acee5a0c0a88ade
xShadowBlade <xshadowblade618@gmail.com>
2026-07-26 18:40:49 -0400

    chore: remove separate examples package

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -2,13 +2,8 @@
  * @file This file is the entry point for your project.
  */
 
-// import { E } from "emath.js";
-
-// console.log(E(69420).format());
-
-import "./nguformat";
-// import "./latexToE";
 import "./coinGame/index";
+import "./nguformat";
 
 void (async (): Promise<void> => {
     const keysToLoad = {
