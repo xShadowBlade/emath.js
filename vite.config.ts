f95ab9ae5aa62bf80c3b4b3f0818bd157df049a0
xShadowBlade <xshadowblade618@gmail.com>
2026-08-03 22:50:41 -0400

    chore: oxfmt on save

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,18 @@
 import dts from "vite-plugin-dts";
 import { defineConfig } from "vite-plus";
+import oxfmtConfig from "./.oxfmtrc.json";
 import packageJson from "./package.json";
 
 export default defineConfig({
-    plugins: [dts({
-        outDirs: "dist/types",
-    })],
+    plugins: [
+        dts({
+            outDirs: "dist/types",
+        }),
+    ],
     define: {
         PKG_VERSION: JSON.stringify(packageJson.version),
     },
-    fmt: {
-        arrowParens: "always",
-        bracketSpacing: true,
-        jsxBracketSameLine: false,
-        jsxSingleQuote: false,
-        printWidth: 120,
-        proseWrap: "preserve",
-        semi: true,
-        singleQuote: false,
-        tabWidth: 4,
-        trailingComma: "all",
-        useTabs: false,
-        sortPackageJson: false,
-        ignorePatterns: [],
-    },
+    fmt: oxfmtConfig as any,
     lint: {
         jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
         rules: { "vite-plus/prefer-vite-plus-imports": "error" },
@@ -33,13 +22,14 @@ export default defineConfig({
         sourcemap: true,
         lib: {
             entry: {
-                "emath": "src/index.ts",
+                emath: "src/index.ts",
                 "emath.game": "src/game/index.ts",
                 "emath.presets": "src/presets/index.ts",
             },
             formats: ["es", "cjs"],
             name: "eMath",
-            fileName: (format, entryName) => `${entryName}.${format === "es" ? "mjs" : format === "cjs" ? "js" : "min.js"}`,
+            fileName: (format, entryName) =>
+                `${entryName}.${format === "es" ? "mjs" : format === "cjs" ? "js" : "min.js"}`,
         },
-    }
+    },
 });
